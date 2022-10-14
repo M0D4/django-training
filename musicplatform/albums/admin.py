1b6962b9d5e87064a17f90af5a112b72b2854fa0
Moustafa Mohamed <darsh.m7md@gmail.com>
2022-10-14 19:35:52 +0200

    Add field "Approved" to album model Add models to django admin make creation time readonly to admin add help text to "Approved" field

--- musicplatform/albums/admin.py
+++ musicplatform/albums/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Album
 
 # Register your models here.
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    fields = ['artist', 'name', 'creation_datetime',
+              'release_datetime', 'cost', 'approved']
+    readonly_fields = ['creation_datetime']
+
+
+admin.site.register(Album, AlbumAdmin)
