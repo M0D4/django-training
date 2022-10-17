29b2c6f2d36b4d94cd2a4c44510e9a56188078d7
Moustafa Mohamed <darsh.m7md@gmail.com>
2022-10-18 01:35:07 +0200

    make order by approved_albums available

--- musicplatform/artists/models.py
+++ musicplatform/artists/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.db.models import Q, Count
+
+
+class ArtistManager(models.Manager):
+    def get_queryset(self):
+        albums = Count('album', filter=Q(album__approved=True))
+        return super().get_queryset().annotate(approved_albums=albums).order_by("approved_albums")
 
 
 class Artist(models.Model):
     stage_name = models.CharField(
         max_length=200, unique=True, blank=False, null=False)
     social_link = models.URLField(max_length=300, null=False, blank=True)
+    objects = ArtistManager()
 
     def __str__(self):
         return self.stage_name
