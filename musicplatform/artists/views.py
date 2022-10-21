cb41c162c9f33de1817fd9c319e402fff080bdd6
Moustafa Mohamed <eng.moustafa.abdelhady@gmail.com>
2022-10-21 21:50:25 +0200

    Create list view for the artists at .../artists

--- musicplatform/artists/views.py
+++ musicplatform/artists/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from artists.models import Artist
 
-# Create your views here.
+app_name = 'artists'
+
+
+def create(request):
+    return HttpResponse("You are adding an artist")
+
+
+def index(request):
+    artist_list = Artist.objects.all()
+    context = {'artist_list': artist_list}
+    return render(request, 'artists/index.html', context)
