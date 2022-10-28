557016f654d26883d456b53989f339dadd2d7a96
Moustafa Mohamed <eng.moustafa.abdelhady@gmail.com>
2022-10-29 00:36:17 +0200

    - prevent admin from deleting all songs from an album

--- musicplatform/albums/admin.py
+++ musicplatform/albums/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Album, Song
 from .forms import SongForm
-from django import forms
+from django.contrib import messages
 
 
 class SongInline(admin.TabularInline):
@@ -20,21 +20,62 @@ class AlbumAdmin(admin.ModelAdmin):
 admin.site.register(Album, AlbumAdmin)
 
 
-class SongAdmin(admin.ModelAdmin):
+class RemoveAdminDefaultMessageMixin:
+
+    def remove_default_message(self, request):
+        storage = messages.get_messages(request)
+        try:
+            del storage._queued_messages[-1]
+        except KeyError:
+            pass
+        return True
+
+    def response_delete(self, request, obj_display, obj_id):
+        """override"""
+        response = super().response_delete(request, obj_display, obj_id)
+        self.remove_default_message(request)
+        return response
+
+    def response_queryset_delete(self, request, obj_display, query_set):
+        """override"""
+        response = super().response_queryset_delete(request, obj_display, query_set)
+        self.remove_default_message(request)
+        return response
+
+
+class SongAdmin(RemoveAdminDefaultMessageMixin, admin.ModelAdmin):
     exclude = ['image_thumbnail']
     form = SongForm
     #readonly_fields = ['image_thumbnail']
 
     def delete_model(self, request, obj):
         if obj.album.song_set.count() == 1:
-            raise forms.ValidationError("Album Can't be empty")
-        obj.delete()
+            self.message_user(
+                request, 'There should be at least 1 song left in each album.', level=messages.ERROR)
+        else:
+            self.message_user(
+                request, 'The song “{}” was deleted successfully.'.format(obj)
+            )
+            obj.delete()
 
-    def get_actions(self, request):
-        actions = super().get_actions(request)
-        if 'delete_selected' in actions:
-            del actions['delete_selected']
-        return actions
+    def delete_queryset(self, request, queryset):
+        freq = {}
+        for song in queryset:
+            if song.album.id in freq:
+                freq[song.album.id] += 1
+            else:
+                freq[song.album.id] = 1
+
+        can_delete = True
+        for id in freq:
+            if Album.objects.get(pk=id).song_set.count() - freq[id] < 1:
+                can_delete = False
+
+        if can_delete:
+            return super().delete_queryset(request, queryset)
+        else:
+            self.message_user(
+                request, 'There should be at least 1 song left in each album.', level=messages.ERROR)
 
 
 admin.site.register(Song, SongAdmin)
