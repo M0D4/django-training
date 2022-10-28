c24b46f22e67f6e302832f94a09949237215b54c
Moustafa Mohamed <darsh.m7md@gmail.com>
2022-10-28 23:16:43 +0200

    - Make the admin can't create an album without first uploading a song. - Prevent admin from deleting all songs of an album

--- musicplatform/albums/admin.py
+++ musicplatform/albums/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
 from .models import Album, Song
 from .forms import SongForm
+from django import forms
+
+
+class SongInline(admin.TabularInline):
+    model = Song
+    extra = 0
+    min_num = 1
 
 
 class AlbumAdmin(admin.ModelAdmin):
     fields = ['artist', 'name', 'created',
               'released', 'cost', 'approved']
     readonly_fields = ['created']
+    inlines = [SongInline]
 
 
 admin.site.register(Album, AlbumAdmin)
@@ -16,6 +24,17 @@ class SongAdmin(admin.ModelAdmin):
     exclude = ['image_thumbnail']
     form = SongForm
     #readonly_fields = ['image_thumbnail']
+
+    def delete_model(self, request, obj):
+        if obj.album.song_set.count() == 1:
+            raise forms.ValidationError("Album Can't be empty")
+        obj.delete()
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
 
 
 admin.site.register(Song, SongAdmin)
