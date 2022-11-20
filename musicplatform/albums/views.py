995dcd25f1e90a040b6df107f5fe788209f811d6
Moustafa Mohamed <darsh.m7md@gmail.com>
2022-11-20 19:28:38 +0200

    - Create album filters that support Cost greater than or equal, Cost less than or equal, Case-insensitive containment

--- musicplatform/albums/views.py
+++ musicplatform/albums/views.py
@@ -1,8 +1,9 @@
-from albums.models import Album
+from .models import Album
 from .serializers import AlbumSerializer
 from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from users.permissions import IsArtist
+from .filters import AlbumFilter
 
 app_name = 'albums'
 
@@ -11,6 +12,7 @@ class AlbumList(generics.ListCreateAPIView):
     queryset = Album.objects.get_approved_albums()
     serializer_class = AlbumSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsArtist]
+    filterset_class = AlbumFilter
 
     def create(self, request, format=None):
         serializer = AlbumSerializer(data=request.data)
