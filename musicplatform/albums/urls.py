df63defb3ac7dd4a1c169151573b4496455d89ad
Moustafa Mohamed <darsh.m7md@gmail.com>
2022-11-20 23:15:58 +0200

    - Create view AlbumListManualFilter

--- musicplatform/albums/urls.py
+++ musicplatform/albums/urls.py
@@ -4,6 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('albums/', views.AlbumList.as_view(), name='albums'),
+    path('albums/manual-filter/',
+         views.AlbumListManualFilter.as_view(), name='albums-manual-filter'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
