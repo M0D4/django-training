c80d09da7c2e44d8bf9f2b61a9c67853f6e5a3f9
Moustafa Mohamed <darsh.m7md@gmail.com>
2022-11-20 18:23:59 +0200

    - Generalize Album serializer

--- musicplatform/albums/serializers.py
+++ musicplatform/albums/serializers.py
@@ -1,22 +1,11 @@
 from rest_framework import serializers
 from albums.models import Album
 from artists.serializers import ArtistSerializer
-from artists.models import Artist
 
 
-class AlbumGetSerializer(serializers.ModelSerializer):
-    artist = ArtistSerializer()
+class AlbumSerializer(serializers.ModelSerializer):
+    artist = ArtistSerializer(read_only=True)
 
     class Meta:
         model = Album
         fields = ['id', 'artist', 'name', 'released', 'cost']
-
-
-class AlbumPostSerializer(serializers.ModelSerializer):
-
-    def create(self, validated_data):
-        return Album.objects.create(**validated_data)
-
-    class Meta:
-        model = Album
-        fields = ['name', 'released', 'cost', 'approved']
--- musicplatform/musicplatform/settings.py
+++ musicplatform/musicplatform/settings.py
@@ -146,7 +146,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.BasicAuthentication',
                                        'rest_framework.authentication.SessionAuthentication',
                                        'knox.auth.TokenAuthentication',
-                                       )
+                                       ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 REST_KNOX = {
